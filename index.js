10e94c70124994a0f3502d5f20c3b6d472be4e20
huzaif shaikh <59112665+huzaifsk@users.noreply.github.com>
2022-12-13 21:49:47 +0530

    added some practise sessions while learning react with  DOM, jsx and babel

---
+++ index.js
@@ -0,0 +1,109 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is a paragraph.</p>
+  </div>,
+  document.getElementById("root")
+);
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>Bacon</li>
+      <li>Noodles</li>
+      <li>Jamon</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+
+const fName = "Angela";
+const lName = "Yu";
+const num = 7;
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {fName + " " + lName}!</h1>
+    <p>Your lucky number is {num}</p>
+  </div>,
+  document.getElementById("root")
+);
+
+const name = "Angela";
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+
+ReactDOM.render(
+  <div>
+    <p>Created by {name}</p>
+    <p>Copyright {year}</p>
+  </div>,
+  document.getElementById("root")
+);
+
+const img = "https://picsum.photos/200";
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading">My Favourite Foods</h1>
+    <img alt="random" src={img + "?grayscale"} />
+
+    <img
+      className="food-img"
+      alt="bacon"
+      src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-190621-air-fryer-bacon-0035-landscape-pf-1567632709.jpg?crop=0.645xw:0.967xh;0.170xw,0.0204xh&resize=480:*"
+    />
+    <img
+      className="food-img"
+      alt="jamon"
+      src="https://images-na.ssl-images-amazon.com/images/I/71lNrnbMXsL._SL1200_.jpg"
+    />
+    <img
+      className="food-img"
+      alt="noodles"
+      src="https://www.errenskitchen.com/wp-content/uploads/2014/04/quick-and-easy-chinese-noodle-soup3-1.jpg"
+    />
+  </div>,
+  document.getElementById("root")
+);
+
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black"
+};
+
+customStyle.color = "blue";
+
+ReactDOM.render(
+  <h1 style={customStyle}>Hello World!</h1>,
+  document.getElementById("root")
+);
+
+const currDate = new Date(2019, 1, 1, 40);temp
+
+let greeting;
+
+const customStyle = {
+  color: ""
+};
+
+if (currTime < 12) {
+  greeting = "Good Morning";
+  customStyle.color = "red";
+} else if (currTime < 18) {
+  greeting = "Good Afternoon";
+  customStyle.color = "blue";
+} else {
+  greeting = "Good Night";
+  customStyle.color = "grey";
+}
+
+console.log(currTime);
+
+ReactDOM.render(<h1 style= {customStyle}> {greeting} </h1>, document.getElementById("root"));
+
